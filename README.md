5c927c400b978bfc19107d8ed16c07d15461e40b
Chlaxi <chlaxi.lunari@gmail.com>
2020-04-21 17:15:07 +0200

    Basic Movement
    Basic movement for both keyboard and mouse controls
    
    Sample world added as well

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Stormheim_Valley
+
+
+The player sprite is a temporary sprite used for testing.
+
+The overworld tileset currently used, is made by Buch (https://opengameart.org/users/buch) from OpenGameArt and can be found at https://opengameart.org/content/overworld-tiles-0
+who made the tilemap consulting with Jeffrey Kern.
---
+++ Stormheim Valley/Assets/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D),typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    public bool pointerMovement;
+    public float speed = 2f;
+
+    Vector2 dir;
+   // public LayerMask layerMasks;
+
+
+    Rigidbody2D rg;
+    Animator animator;
+
+    [SerializeField]
+    private Vector3 mousePosition;
+    
+    
+    private void Start()
+    {
+        rg = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+        if (pointerMovement && Input.GetMouseButton(0))
+        {
+            FaceCursor();
+        }
+        else
+        {
+
+            dir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        }
+    }
+
+
+    private void FaceCursor()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //Update distance checking
+        if (Vector2.Distance(mousePosition, rg.position)<0.5f)
+        {
+            Debug.Log("cursor on player");
+            dir = new Vector2(0, 0);
+            return;
+        }
+
+        dir = new Vector2(
+            mousePosition.x - transform.position.x,
+            mousePosition.y - transform.position.y).normalized;
+
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = dir * speed;
+        rg.MovePosition(rg.position + velocity * Time.deltaTime);
+        animator.SetFloat("Horizontal", dir.x);
+        animator.SetFloat("Vertical", dir.y);
+        animator.SetFloat("Speed", velocity.magnitude);
+        
+        //TODO: Keeps animation face direction
+
+    }
+
+}
